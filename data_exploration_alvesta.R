3893876c9333ca80980ab39aed38d20387ef3dcf
Niko Stratil <niko.stratil@gmail.com>
2024-01-22 17:25:16 +0100

    Modeling arrival delay of second train and which transfer can be reached

--- data_exploration_alvesta.R
+++ data_exploration_alvesta.R
@@ -3,27 +3,34 @@ library(tidyverse)
 library(readr)
 library(ggplot2)
 
-connections_av = read.fst("~/Thesis/Data_Niko/2024-01-09/Av/connections.fst")
-lines_av = read_csv("~/Thesis/Data_Niko/2024-01-09/Av/lines.csv")
+connections_av = read.fst("~/Thesis/Data_Niko/2024-01-09-v2/Av/connections.fst")
+lines_av = read_csv("~/Thesis/Data_Niko/2024-01-09-v2/Av/lines.fst")
+lines_av_vo = lines_av %>% filter(str_detect(gtfs.locations, "VÖ"))
 
 # leave in only trains with Linköping in their route
 connections_av = connections_av %>% filter(str_detect(arr.gtfs.locations, "LP"))
 # keep only trains with direction from Linköping
-connections_av_from_lp = connections_av %>% filter(arr.direction == 1)
-# TODO filter only to connections to Växjö!!
+connections_av_from_lp = connections_av %>% filter((arr.line.group == 6 & arr.direction == 1))
+# filter only to connections to Växjö
+connections_av_from_lp = connections_av_from_lp %>% filter(str_detect(dep.gtfs.locations, "VÖ") & dep.direction == 2)
 
 ggplot(connections_av_from_lp, aes(x=arr.Operator)) + geom_bar()
 hist(as.numeric(connections_av_from_lp$arr.ArrivalDelay))
 mean(connections_av_from_lp$arr.ArrivalDelay)
 
-trains_av = read.fst("~/Thesis/Data_Niko/2024-01-09/Av/trains.fst")
+# grouping all transfers per run to see how many first transfers were reached
+selected_data = connections_av_from_lp %>% select(arr.ActivityId, Reached, PlannedTransferTime) %>% arrange(PlannedTransferTime)
 
-trains_va = read.fst("~/Thesis/Data_Niko/2024-01-09/Vö/trains.fst")
+grouped_arrivals <- selected_data %>% 
+  group_by(arr.ActivityId) %>% 
+  mutate(reached_number = row_number())
 
-# filter out trains without arrival delay (trains starting at Växjö)
-trains_va = trains_va %>% filter(!is.na(ArrivalDelay))
-# filter out trains going to Alvesta (alvesta in ViaToLocation)
-trains_va = trains_va %>% filter(!str_detect(ViaToLocation, "Av"))
+reshaped_data <- grouped_arrivals %>% 
+  pivot_wider(names_from = reached_number, values_from = Reached, names_prefix = "Reached.") 
 
-mean(as.numeric(trains_va$ArrivalDelay))
-hist(as.numeric(trains_va$ArrivalDelay), breaks = 50)
+hist(as.numeric(reshaped_data %>% filter(!is.na(Reached.1)) %>% pull(PlannedTransferTime)))
+hist(as.numeric(reshaped_data %>% filter(!is.na(Reached.2)) %>% pull(PlannedTransferTime)))
+hist(as.numeric(reshaped_data %>% filter(!is.na(Reached.3)) %>% pull(PlannedTransferTime)))
+hist(as.numeric(reshaped_data %>% filter(!is.na(Reached.4)) %>% pull(PlannedTransferTime)))
+
+
--- modelling.R
+++ modelling.R
@@ -1,7 +1,7 @@
 # sample of a mixture model of log normal distribution and multinomial classes
 
-samples = rlnorm(1000, sdlog = 0.75)
-hist(samples)
+samples = rlnorm(1000, sdlog = 0.8) - 1
+hist(samples, breaks = 50)
 
 pr1 = 0.7
 pr2 = 0.2
@@ -11,3 +11,14 @@ arrDelay = samples + t(c(0,20,40)) %*% rmultinom(1000,1,c(0.7,0.2,0.1))
 hist(arrDelay)
 mean(arrDelay)
 median(arrDelay)
+
+# Modelling distribution arrival of second train
+trains_va = read.fst("~/Thesis/Data_Niko/2024-01-09-v2/Vö/trains.fst")
+
+# filter out trains without arrival delay (trains starting at Växjö)
+trains_va = trains_va %>% filter(!is.na(ArrivalDelay))
+# filter out trains going to Alvesta (alvesta in ViaToLocation)
+trains_va = trains_va %>% filter(!str_detect(ViaToLocation, "Av"))
+
+mean((trains_va$ArrivalDelay))
+hist(as.numeric(trains_va$ArrivalDelay), breaks = 50, main = "Histogram of arrivals in Växjö")
